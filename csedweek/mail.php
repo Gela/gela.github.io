119831c8b4351108af8f0f74ebf95b552cbfa759
unknown <cast.gelarehm@cast-gelarehm.caset.buffalo.edu>
2015-11-06 16:13:51 -0500

    copyright

--- csedweek/mail.php
+++ csedweek/mail.php
@@ -3,7 +3,7 @@
 	$email = $_POST['email'];
 	$message = $_POST['message'];
 	$formcontent="From: $name \n Message: $message";
-	$recipient = "gelarehm@buffalo.edu";
+	$recipient = "cse-edweek@buffalo.edu";
 	$subject = "UB CSE Week - Contact Form";
 	$mailheader = "From: $email \r\n";
 	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
